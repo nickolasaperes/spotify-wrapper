6a1dd30d763ded25b8bd785e4502640af5b5894f
nickolasaperes <nickolasaperes@gmail.com>
2020-07-28 23:03:23 -0300

    Added mocha and chai lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    mocha: true
   },
   extends: [
     'airbnb-base',
@@ -14,6 +15,11 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    "chai-friendly"
+  ],
   rules: {
+    "no-unused-expressions": 0,
+    "chai-friendly/no-unused-expressions": 2
   },
 };
